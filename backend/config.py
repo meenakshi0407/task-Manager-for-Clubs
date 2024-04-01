6d434bc39daf48173fa3f0e56cdf3624520c66f9
meenakshi0407 <meenakshideshpande21@gmail.com>
2024-04-01 22:19:52 +0530

    a

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,7 @@ from flask_cors import CORS
 
 app=Flask(__name__)
 CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///tasksdatabase.db"
+app.config["SQLALCHEMY_TRACK_MADIFICATIONS"]=False
+db=SQLAlchemy(app)
