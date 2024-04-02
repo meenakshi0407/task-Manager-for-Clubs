e37eb7f36ecccfbacd8002208408895aab72ff6f
meenakshi0407 <meenakshideshpande21@gmail.com>
2024-04-02 20:02:43 +0530

    backend completed

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,52 @@ def get_tasks():
     json_tasks=list(map(lambda x:x.to_json(),tasks))
     return jsonify({"tasks":json_tasks})
 
+@app.route("/create_tasks",methods=["POST"])
+def create_task():
+    name=request.json.get("name")
+    team=request.json.get("team")
+    task=request.json.get("task")
+
+    if not name  or not  team or not task:
+        return(
+            jsonify({"message": "you must include name,team name and task"}),400,
+        )
+    new_task=Tasks(name=name,team=team,task=task)
+    try:
+        db.session.add(new_task)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message":str(e)}),400
+    
+    return jsonify({"message":"task created! "}),201
+
+@app.route("/update_task/<int:student_id>",methods=["PATCH"])
+def update_task(student_id):
+    task=Tasks.query.get(student_id)
+
+    if not task:
+        return jsonify({"message":"student not found!"}),404
+    
+    data=request.json
+    task.name=data.get("name",Tasks.name)
+    task.team=data.get("team",Tasks.team)
+    task.task=data.get("task",Tasks.task)
+
+    db.session.commit()
+
+    return jsonify({"message":"student updated"}),200
+
+@app.route("/delete_task/<int:student_id>",methods=["DELETE"])
+def delete_task(student_id):
+    task=Tasks.query.get(student_id)
+
+    if not task:
+        return jsonify({"message":"student not found!"}),404
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({"message":"student deleted!"}),200
 
 
 if __name__=="__main__":
